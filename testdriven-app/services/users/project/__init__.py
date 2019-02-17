4fdf04cfeaf7ff694a2703c7655c6182f18e4b93
Ubuntu <ubuntu@ip-172-31-95-55.ec2.internal>
2019-02-17 00:51:20 +0000

    Setup postgres, automated tests, and flask blueprints.

--- testdriven-app/services/users/project/__init__.py
+++ testdriven-app/services/users/project/__init__.py
@@ -1,15 +1,32 @@
-from flask import Flask, jsonify
+# services/users/project/__init__.py
 
-# instantiate the app
-app = Flask(__name__)
+import os 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-# set config
-app.config.from_object('project.config.DevelopmentConfig') # new
+# instantiate the db
+db = SQLAlchemy()
 
-@app.route('/flask/users/ping', methods=['GET'])
-def ping_pong():
-  return jsonify({
-    'status': 'success',
-    'message': 'pong!'
-  })
+def create_app(script_info=None):
+
+    # instantiate the app
+    app = Flask(__name__)
+
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    # set up extensions
+    db.init_app(app)
+
+    # register blueprints
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
+
+    return app
 
